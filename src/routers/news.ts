4f70aa06659f4feae4246bde07eedebdbb40b1ec
kxkaro <kar.krajewska@gmail.com>
2021-06-27 12:13:12 +0200

    add prettier and husky scripts

--- src/routers/news.ts
+++ src/routers/news.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import { getResults } from '../search/searchHTML';
+import express from "express";
+import { getResults } from "../search/searchHTML";
 import { SearchResult } from "../logic/types";
 
 const router = express.Router();
 
-router.get('/', (req: any, res: any) => {
-
-    getResults('news', 1, 'lang_en')
-    .then((data: SearchResult) => res.status(200).send(data));
+router.get("/", (req: any, res: any) => {
+  getResults("news", 1, "lang_en").then((data: SearchResult) =>
+    res.status(200).send(data)
+  );
 });
 
-export default router;  
+export default router;
