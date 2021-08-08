da96a5df62a4e582ea9262e679df74eb807df779
spookydoodle <kxkaro@gmail.com>
2021-08-08 18:48:29 +0200

    Add create db and table if not exist methods on db init

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,73 @@
+/* 
+    On server and db init, first run one pool and create 
+    the needed data base and table, if don't exist.
+    Then close the connection and expose the pool configures 
+    with existing db and table.
+*/
 import { Pool } from "pg";
-import { DB_NAME } from "./db/constants";
+import { DB_NAME, NEWS_TABLE } from "./db/constants";
 
-const pool = new Pool({
-    user: "postgres",
-    password: process.env.DATABASE_PASS,
-    host: "localhost",
-    port: 5432,
-    database: DB_NAME,
-});
+const config = {
+  user: "postgres",
+  password: process.env.DATABASE_PASS,
+  host: process.env.DATABASE_HOST || "localhost",
+  port: 5432,
+};
 
-export default pool;
+const qCreateDb = `CREATE DATABASE ${DB_NAME};`;
+
+const qCreateTb = `CREATE TABLE ${NEWS_TABLE}(
+    id SERIAL PRIMARY KEY,
+    category VARCHAR(20),
+    lang VARCHAR(7),
+    headline VARCHAR(150),
+    provider VARCHAR(40),
+    url VARCHAR(255),
+    timestamp TIMESTAMP
+);`;
+
+const initializeDb = (): Promise<Pool> =>
+  new Promise(async (resolve, reject) => {
+    // Connect to new pool
+    let pool = new Pool(config);
+
+    pool.connect();
+
+    // Create DB if doesn't exist; then end pool and re-connect again on the right db to create table
+    await pool.query(qCreateDb, (dbErr) => {
+      if (!dbErr || dbErr.message.indexOf("already exists") != -1) {
+        console.log(
+          `Data base ${DB_NAME} ${!dbErr ? "created" : "already exists"}.`
+        );
+        
+        pool.end();
+
+        // Connect to the right database and create table if doesn't exist
+        pool = new Pool({ ...config, database: DB_NAME});
+
+        pool.query(
+          qCreateTb,
+          (tbErr) => {
+            if (!tbErr || tbErr.message.indexOf("already exists") != -1) {
+              console.log(
+                `Table base ${NEWS_TABLE} ${
+                  !tbErr ? "created" : "already exists"
+                }.`
+              );
+
+              // Resolve by returning the pool; no need to end
+              resolve(pool);
+            } else {
+              reject(tbErr);
+              pool.end();
+            }
+          }
+        );
+      } else {
+        reject(dbErr);
+        pool.end();
+      }
+    });
+  });
+
+export { initializeDb };
--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,27 @@
 //
 // Note, that this file should be kept as simple as possible, as it is not
 // covered by unit tests.
-
-// Start express app
 import dotenv from "dotenv";
 dotenv.config();
 
-import pool from "./db";
+import { initializeDb } from "./db";
 import app from "./app";
-// import connectToDb from "./dbconfig/dbConnector";
 import Scraper from "./data/scraper";
 import { getAllResults } from "./search/searchHTML";
 import { postNewsData } from "./db/postNewsData";
-import { Headlines } from './logic/types';
+import { Headlines } from "./logic/types";
 
-// Connect to postgreSQL db
-// connectToDb();
-pool.connect();
-
+// Create db and table if they don't exist; then connect
 // Create an instance of the basic news scraper.
 // Initialize method sets data fetch to run once every 24 hrs
 // and to save results in postgreSQL data base
-const scraper = new Scraper("news", getAllResults, (data: Headlines) => postNewsData(pool, data));
-scraper.initialize();
+initializeDb()
+  .then((pool) => {
+    const postNewsToDb = (data: Headlines) => postNewsData(pool, data);
+    const scraper = new Scraper("news", getAllResults, postNewsToDb);
+    scraper.initialize();
+  })
+  .catch((err) => console.log(err));
 
 // Run express app
 const PORT = process.env.PORT || 5000;
