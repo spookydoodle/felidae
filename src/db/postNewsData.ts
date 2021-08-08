1b3d7d8e2848ad8906d28c0fa890597d32332644
spookydoodle <kxkaro@gmail.com>
2021-08-08 13:54:51 +0200

    handle data types in post method

--- src/db/postNewsData.ts
+++ src/db/postNewsData.ts
@@ -8,16 +8,22 @@ export const postNewsData = async (pool: Pool, data: Headlines) => {
 
   for (const { headline, provider, url, timestamp } of data) {
     try {
-      if (headline.length <= 150) {
+      if (headline.length <= 150 && url.length <= 255) {
         const item = await pool.query(
           `INSERT INTO ${NEWS_TABLE}(headline, provider, url, timestamp) VALUES ($1, $2, $3, to_timestamp($4)) RETURNING *;`,
-          [headline, provider, url, timestamp]
+          // if provider length > 40 (as defined in table column data type), 
+          // then take first 40 char to avoid errors
+          [headline, provider.substring(0, 40), url, timestamp]
         );
         items.push(item.rows);
         console.log("Added item to db");
       } else {
         console.log(
-          `Headline: "${headline}" not added due to its length of ${headline.length} char (max 150)`
+          `Headline: "${headline}" not added due to ${
+            headline.length > 150
+              ? `its length of ${headline.length} char (max 150)`
+              : `its source url's length of ${url.length} (max 255)`
+          }`
         );
       }
     } catch (err) {
