1426c0ba175e9047e6120cac6cb861929d5a939e
spookydoodle <kxkaro@gmail.com>
2021-08-08 17:08:28 +0200

    handle new fields in sql inserts

--- src/db/postNewsData.ts
+++ src/db/postNewsData.ts
@@ -6,9 +6,8 @@ const qUrlExists = (url: string) =>
   `SELECT COUNT(*) FROM ${NEWS_TABLE} WHERE url = '${url}';`;
 
 const qInsert = () =>
-  `INSERT INTO ${NEWS_TABLE}(headline, provider, url, timestamp) VALUES (
-      $1, 
-      $2, $3, to_timestamp($4)) RETURNING *;`;
+  `INSERT INTO ${NEWS_TABLE}(category, lang, headline, provider, url, timestamp) 
+  VALUES ($1, $2, $3, $4, $5, to_timestamp($6)) RETURNING *;`;
 
 export const postNewsData = async (pool: Pool, data: Headlines) => {
   const items: Headlines[] = [];
@@ -16,7 +15,7 @@ export const postNewsData = async (pool: Pool, data: Headlines) => {
 
   // Before inserting make sure data meets data type criteria defined in postgres table
   // Also, make sure entry does not exist yet; if it does, then skip it
-  for (const { headline, provider, url, timestamp } of data) {
+  for (const { category, lang, headline, provider, url, timestamp } of data) {
     if (headline.length <= 150 && url.length <= 255) {
       await pool
         .query(qUrlExists(url))
@@ -25,6 +24,8 @@ export const postNewsData = async (pool: Pool, data: Headlines) => {
           if (rows[0].count == 0) {
             await pool
               .query(qInsert(), [
+                category,
+                lang,
                 headline,
                 provider.substring(0, 40),
                 url,
@@ -48,6 +49,8 @@ export const postNewsData = async (pool: Pool, data: Headlines) => {
     }
   }
 
-  console.log(`Added ${items.length} items to ${NEWS_TABLE} table in ${DB_NAME} data base. ${duplicateCount} duplicate url's omitted.`);
+  console.log(
+    `Added ${items.length} items to ${NEWS_TABLE} table in ${DB_NAME} data base. ${duplicateCount} duplicate url's omitted.`
+  );
   return items.flat();
 };
