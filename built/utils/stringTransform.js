cbcd5eb76b423d3cfe1a023624538e3a93819661
spookydoodle <kxkaro@gmail.com>
2021-08-22 12:34:17 +0200

    remove built folder

--- built/utils/stringTransform.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.capitalize = void 0;
-var capitalize = function (text) {
-    return text.charAt(0).toUpperCase() + text.substring(1).toLowerCase();
-};
-exports.capitalize = capitalize;
